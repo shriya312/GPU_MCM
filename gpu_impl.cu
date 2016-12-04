c2f59cc7d9779b3225e8430c68b9ca7416cac6b4
Shriya Nair <shriya.nair@utexas.edu>
2016-12-03 22:41:02 -0600

    corrected copying

--- gpu_impl.cu
+++ gpu_impl.cu
@@ -43,14 +43,14 @@ __global__ void monteCarlopi (int *din) {
 		sm[b] = 0; 
 	// wait for all threads to finish
 
-	__syncthreads();
-	if (thid == 0) {
-	//	printf (" rand %d %f ",  x1, x);
-		for (int i =0;i<2*THREADS;i++)
-			printf("%d,",sm[i]);
+	//__syncthreads();
+	//if (thid == 0) {
+	////	printf (" rand %d %f ",  x1, x);
+	//	for (int i =0;i<2*THREADS;i++)
+	//		printf("%d,",sm[i]);
 
-	}
-	__syncthreads();
+	//}
+	//__syncthreads();
 
 	// find sum of points inside the rectangle using sum function
 
@@ -70,7 +70,7 @@ __global__ void monteCarlopi (int *din) {
 		sm[idx] += sm[idx + 4]; 
 		
 	}
- 	__syncthreads();	
+	__syncthreads();
 
 	if (thid < 16) { // 16 elements
 		idx = thid*16;
@@ -99,16 +99,17 @@ __global__ void monteCarlopi (int *din) {
 	if (thid == 0) { //  1 elements 
 		sm[idx] += sm[idx+128];
 		din[blockIdx.x] = sm[a];
-		printf (" sum %d", sm[a]);	
+		//printf ("sum : %d \n ", sm[idx]);
+//		printf (" sum %d", sm[a]);	
 	}
 
 }
 
 
-__global__ void prefixSum ( int *d_in , int *sum, int length) {
+__global__ void prefixSum ( int *d_in , int *temp, int length) {
 
 
-	extern __shared__ int sm[];
+	__shared__ int sm[2*THREADS];
 	int thid = threadIdx.x;
 	int t = blockIdx.x*blockDim.x + threadIdx.x;
 	int a = 2*thid;
@@ -168,48 +169,66 @@ __global__ void prefixSum ( int *d_in , int *sum, int length) {
 
 	if (thid == 0) { //  1 elements 
 		sm[idx] += sm[idx+128];
-		*sum = sm[idx];	
+		temp[blockIdx.x] = sm[idx];
+		//printf ("sum p : %d \n ", sm[idx]);
 	}
-
-
-
 
 }
 
+
+__global__ void copy_kernel (int *out , int *in, int length) {
+	int t = threadIdx.x + blockIdx.x * blockDim.x;
+	if (2*t < length) 
+		out[2*t] = in[2*t];
+	if ((2*t + 1) < length)
+		out[2*t + 1] = in[2*t + 1];
+
+}
 
 void monteCarlopi(int num_blocks, int length, double & pi_val) {
 
 	// store the count in global array
-	int *d_in, *sum;
+	int *d_in, *temp;
+	int  i = 0;
 	cudaMalloc((void**)&d_in, num_blocks*sizeof(int));
-	cudaMalloc((void**)&sum, sizeof(int));
-	printf ("num blocks %d ", num_blocks);
+	cudaMalloc((void**)&temp, num_blocks*sizeof(int));
+	//printf ("num blocks %d ", num_blocks);
 	monteCarlopi<<<num_blocks,THREADS>>>(d_in);
-	if (num_blocks <= 2*THREADS) {
-		prefixSum<<<1, THREADS>>>(d_in, sum, num_blocks);
-	} else {
-		
-		int num_blocks1 = num_blocks/(2*THREADS);
-		while (num_blocks1 > 1) {
-			num_blocks1 = num_blocks/(2*THREADS);
-			prefixSum<<<num_blocks1,THREADS>>>(d_in, sum, num_blocks);
-			num_blocks = num_blocks1;
-		}
-		prefixSum<<<1,THREADS>>>(d_in, sum, num_blocks);
+	if (num_blocks == 1) {
 	}
+	else {
+		int num_blocks1 = num_blocks/(2*THREADS);
+		if (num_blocks1 < 1) num_blocks1 = 1;
+		while (num_blocks > 1) {
+			if (i%2 == 0)  
+				prefixSum<<<num_blocks1,THREADS>>>(d_in, temp, num_blocks);
+			else 
+				prefixSum<<<num_blocks1,THREADS>>>(temp, d_in, num_blocks);
+			i++;
+			num_blocks = num_blocks1;
+			num_blocks1 = num_blocks/(2*THREADS);
+			if (num_blocks1 < 1) num_blocks1 = 1;
+			//copy_kernel<<<num_blocks1, THREADS>>> (d_in, temp, num_blocks);
+		}
+	}
+	int total_sum;
+	if (i%2 == 1)
+		cudaMemcpy(&total_sum, &temp[0], sizeof(int), cudaMemcpyDeviceToHost );
+	else
+		cudaMemcpy(&total_sum, &d_in[0], sizeof(int), cudaMemcpyDeviceToHost );
+	//printf ("total sum : %d ", total_sum);	
+	pi_val = (double) 4*total_sum/ double (length);	
 	cudaFree(d_in);
-	int h_sum[1];
-	cudaMemcpy(h_sum, sum, sizeof(int), cudaMemcpyDeviceToHost );	
-	cudaFree(sum);	
-	double pi = 4*(double)h_sum[0]/(double)length;
 }
 
 int main(int argc,char **argv)
 {
-	int length = 256;
+	int length = 512;
 	double pi_val = 0;
 
 	int num_blocks = length/(2*THREADS);
 
-	monteCarlopi(num_blocks,length,pi_val); 	
+	monteCarlopi(num_blocks,length,pi_val); 
+
+	printf ("Value of pi : %f\n ", pi_val);	
 }
